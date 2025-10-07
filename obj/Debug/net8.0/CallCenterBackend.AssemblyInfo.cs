c4113f93a13fb3c3d8305efc8ef0bf0c8e2906eb
Manuel Sarmiento <manuseb2001@gmail.com>
2025-10-07 16:01:00 -0500

    Actualización del backend con las nuevas modificaciones

--- obj/Debug/net8.0/CallCenterBackend.AssemblyInfo.cs
+++ obj/Debug/net8.0/CallCenterBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CallCenterBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4dff5627bc7e393df65413411ed3e745ccaa8783")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd7f9cd32ab5dcfdc490c3fdee7165d78f834119")]
 [assembly: System.Reflection.AssemblyProductAttribute("CallCenterBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CallCenterBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
